b4b6b425e5dccb712d0640581b1835b0a968bf41
Herry Gouw <herrygouw@gmail.com>
2024-06-27 22:00:52 +0700

    fix using uuid

--- sai_ocr_batch_complete/models/dms_file.py
+++ sai_ocr_batch_complete/models/dms_file.py
@@ -8,17 +8,26 @@ import time
 class File(models.Model):
     _inherit = "dms.file"
 
+    batch_processing = fields.Boolean()
+
     def action_send_ocr(self):
-        # if not self.env.context.get("test_queue_job_no_delay", False):
-        results = []
-        log_error = ""
-        for record in self:
+        self.ensure_one()
+        if not self.batch_processing:
+            return super().action_send_ocr()
+        if not self.env.context.get("job_uuid") and not self.env.context.get(
+            "test_queue_job_no_delay"
+        ):
             description = _(
                 "Send OCR with id {rec_id} to {file_name}"
             ).format(
-                rec_id=record.id,
-                file_name=record.name,
+                rec_id=self.id,
+                file_name=self.name,
             )
-            record.with_delay(description=description).action_send_ocr()
-        return results, log_error
+            job = self.with_delay(description=description).action_send_ocr()
+            return "Send OCR with uuid {}".format(job.uuid)
+        else:
+            return super(
+                File, self.with_context(asset_batch_processing=True)
+            ).action_send_ocr()
+
 
