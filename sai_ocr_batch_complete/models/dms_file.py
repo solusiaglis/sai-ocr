28d75891dd9e458406f6e827b99c2fe5984f7107
Herry Gouw <herrygouw@gmail.com>
2024-06-27 22:12:43 +0700

    fix again for multiple

--- sai_ocr_batch_complete/models/dms_file.py
+++ sai_ocr_batch_complete/models/dms_file.py
@@ -12,22 +12,23 @@ class File(models.Model):
 
     def action_send_ocr(self):
         self.ensure_one()
-        if not self.batch_processing:
-            return super().action_send_ocr()
-        if not self.env.context.get("job_uuid") and not self.env.context.get(
-            "test_queue_job_no_delay"
-        ):
-            description = _(
-                "Send OCR with id {rec_id} to {file_name}"
-            ).format(
-                rec_id=self.id,
-                file_name=self.name,
-            )
-            job = self.with_delay(description=description).action_send_ocr()
-            return "Send OCR with uuid {}".format(job.uuid)
-        else:
-            return super(
-                File, self.with_context(asset_batch_processing=True)
-            ).action_send_ocr()
+        for rec in self:
+            if not rec.batch_processing:
+                return super().action_send_ocr()
+            if not self.env.context.get("job_uuid") and not self.env.context.get(
+                "test_queue_job_no_delay"
+            ):
+                description = _(
+                    "Send OCR with id {rec_id} to {file_name}"
+                ).format(
+                    rec_id=rec.id,
+                    file_name=rec.name,
+                )
+                job = rec.with_delay(description=description).action_send_ocr()
+                return "Send OCR with uuid {}".format(job.uuid)
+            else:
+                return super(
+                    File, rec.with_context(asset_batch_processing=True)
+                ).action_send_ocr()
 
 
